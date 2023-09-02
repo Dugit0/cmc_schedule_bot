4a43f5a0839b7ddb2dad4c9ce94216b5c2054c2d
Dugit0 <dmitry.astrakhantsev@gmail.com>
2023-09-02 14:42:49 +0300

    Add Phil237 and mod changing

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from datetime import timezone
 import datetime
 import config
 import telebot
+import sys
 import os
 import logging
 
@@ -12,29 +13,41 @@ schedules_path = "schedules"
 # get filenames of schedules and cut ".txt"
 allouds_mods = list(map(lambda name: name[:-4], os.listdir(schedules_path)))
 # set default mod
-current_bot_mod = allouds_mods[0]
+mods_for_chats = dict()
 
 
 def open_schedule():
     """
     Returns
     -------
-    schedule : list of strings
-        list of schedules for each day.
-
+    schedule : dict in format <string> : <list of strings>
+        dict in format
+            key - mod
+            value - list of schedules for each day
     """
-    global schedules_path, current_bot_mod
-    with open(os.path.join(schedules_path, current_bot_mod + ".txt"), encoding="utf-8") \
-            as schedule_file:
-        lines = schedule_file.readlines()
-    schedule = [""] * 7
-    count = -1
-    for i in range(len(lines)):
-        if lines[i] != "#\n":
-            schedule[count] += lines[i]
-        else:
-            count += 1
+    global schedules_path, allouds_mods
+    schedule = dict()
+    for mod in allouds_mods:
+        with open(os.path.join(schedules_path, mod + ".txt"), encoding="utf-8") \
+                as schedule_file:
+            lines = schedule_file.readlines()
+        schedule[mod] = [""] * 7
+        count = -1
+        for i in range(len(lines)):
+            if lines[i] != "#\n":
+                schedule[mod][count] += lines[i]
+            else:
+                count += 1
     return schedule
+
+
+def get_mod(chat_id):
+    global mods_for_chats
+    try:
+        return mods_for_chats[chat_id]
+    except:
+        mods_for_chats[chat_id] = allouds_mods[0]
+        return allouds_mods[0]
 
 
 schedule = open_schedule()
@@ -50,7 +63,7 @@ def command_today(message):
     logging.info(f"CHAT_ID is {message.chat.id}: Call /today")
     cur_date = datetime.datetime.now(timezone.utc) + datetime.timedelta(hours=3)
     day = cur_date.weekday()
-    bot.send_message(message.chat.id, schedule[day], "Markdown")
+    bot.send_message(message.chat.id, schedule[get_mod(message.chat.id)][day], "Markdown")
 
 
 @bot.message_handler(commands=["tomorrow"])
@@ -58,7 +71,7 @@ def command_tomorrow(message):
     logging.info(f"CHAT_ID is {message.chat.id}: Call /tomorrow")
     cur_date = datetime.datetime.now(timezone.utc) + datetime.timedelta(hours=3)
     day = cur_date.weekday()
-    bot.send_message(message.chat.id, schedule[(day + 1) % 7], "Markdown")
+    bot.send_message(message.chat.id, schedule[get_mod(message.chat.id)][(day + 1) % 7], "Markdown")
 
 
 @bot.message_handler(commands=["any"])
@@ -74,7 +87,7 @@ def command_any(message):
 def command_week(message):
     logging.info(f"CHAT_ID is {message.chat.id}: Call /week")
     for day in range(7):
-        bot.send_message(message.chat.id, schedule[day], "Markdown")
+        bot.send_message(message.chat.id, schedule[get_mod(message.chat.id)][day], "Markdown")
 
 
 @bot.message_handler(commands=["changemod"])
@@ -89,15 +102,14 @@ def command_changemod(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle_query(call):
-    global current_bot_mod, schedule
+    global mods_for_chats, schedule
     if call.data[:3] == "any":
         day = int(call.data[4:]) - 1
-        bot.send_message(call.message.chat.id, schedule[day], "Markdown")
+        bot.send_message(call.message.chat.id, schedule[get_mod(call.message.chat.id)][day], "Markdown")
     elif call.data[:3] == "mod":
-        current_bot_mod = call.data[4:]
-        schedule = open_schedule()
-        bot.send_message(call.message.chat.id, f"Вы выбрали расписание группы {current_bot_mod}")
-        logging.info(f"CHAT_ID is {call.message.chat.id}: Mod changed to {current_bot_mod}")
+        mods_for_chats[call.message.chat.id] = call.data[4:]
+        bot.send_message(call.message.chat.id, f"Вы выбрали расписание группы {mods_for_chats[call.message.chat.id]}")
+        logging.info(f"CHAT_ID is {call.message.chat.id}: Mod changed to {mods_for_chats[call.message.chat.id]}")
 
 
 @bot.message_handler(commands=["names"])
